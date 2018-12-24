747cc7000b7c4aed2af2552fd03036fa67cfceb2
Brock Fanning <brockfanning@gmail.com>
2018-12-24 00:24:47 -0500

    Default to correct base url

--- tests/features/support/mink.js
+++ tests/features/support/mink.js
@@ -2,7 +2,7 @@ const cucumber = require('cucumber');
 const mink = require('cucumber-mink');
 
 const driver = new mink.Mink({
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://127.0.0.1:4000/open-sdg-site-testing/',
   viewport: {
     width: 1366,
     height: 768,
