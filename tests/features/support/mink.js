70ced67622433a728077336476b8a81b8ef6666a
Brock Fanning <brockfanning@gmail.com>
2018-12-24 09:38:53 -0500

    Use no-sandbox option

--- tests/features/support/mink.js
+++ tests/features/support/mink.js
@@ -7,6 +7,9 @@ const driver = new mink.Mink({
     width: 1366,
     height: 768,
   },
+  // Set no-sandbox to work around this problem:
+  // https://github.com/GoogleChrome/puppeteer/issues/290
+  noSandbox: true,
 });
 
 driver.hook(cucumber);
