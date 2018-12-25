1ee0ca8bd1a25337ad015feab59c834da299c20b
Brock Fanning <brockfanning@gmail.com>
2018-12-25 16:35:48 -0500

    Attempt to fix weird failures

--- tests/features/support/mink.js
+++ tests/features/support/mink.js
@@ -2,7 +2,7 @@ const cucumber = require('cucumber');
 const mink = require('cucumber-mink');
 
 const driver = new mink.Mink({
-  baseUrl: 'http://127.0.0.1:4000/',
+  baseUrl: 'http://127.0.0.1:4000',
   viewport: {
     width: 1366,
     height: 768,
