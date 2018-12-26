e808c8f92ff642d1bb3876d14b48134cbabb87f0
Brock Fanning <brockfanning@gmail.com>
2018-12-26 15:25:50 -0500

    Remove unneeded Mink config

--- tests/features/support/mink.js
+++ tests/features/support/mink.js
@@ -7,12 +7,6 @@ const driver = new mink.Mink({
     width: 1366,
     height: 768,
   },
-  // Set no-sandbox to work around this problem:
-  // https://github.com/GoogleChrome/puppeteer/issues/290
-  noSandbox: true,
 });
-
-// Allow a little extra time for web page requests.
-cucumber.setDefaultTimeout(15 * 1000);
 
 driver.hook(cucumber);
