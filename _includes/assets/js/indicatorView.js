19fb525faf2ae9e0f76de4e5e6e4dfba2c5909f4
Brock Fanning <brockfanning@gmail.com>
2019-03-09 21:51:13 -0500

    Use translation.t() in indicator javascript

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -311,7 +311,7 @@ var indicatorView = function (model, options) {
               text.push('<li data-datasetindex="' + datasetIndex + '">');
               text.push('<span class="swatch' + (dataset.borderDash ? ' dashed' : '') + '" style="background-color: ' + dataset.backgroundColor + '">');
               text.push('</span>');
-              text.push(dataset.label);
+              text.push(translations.t(dataset.label));
               text.push('</li>');
             });
 
@@ -421,7 +421,7 @@ var indicatorView = function (model, options) {
 
   this.toCsv = function (tableData) {
     var lines = [],
-    headings = _.map(tableData.headings, function(heading) { return '"' + heading + '"'; });
+    headings = _.map(tableData.headings, function(heading) { return '"' + translations.t(heading) + '"'; });
 
     lines.push(headings.join(','));
 
@@ -487,7 +487,6 @@ var indicatorView = function (model, options) {
     }, table = $(el).find('table');
 
     datatables_options.aaSorting = [];
-
     table.DataTable(datatables_options);
 
     setDataTableWidth(table);
@@ -577,7 +576,7 @@ var indicatorView = function (model, options) {
 
       var getHeading = function(heading, index) {
         var span = '<span class="sort" />';
-        var span_heading = '<span>' + heading + '</span>';
+        var span_heading = '<span>' + translations.t(heading) + '</span>';
         return (!index || heading.toLowerCase() == 'units') ? span_heading + span : span + span_heading;
       };
 
