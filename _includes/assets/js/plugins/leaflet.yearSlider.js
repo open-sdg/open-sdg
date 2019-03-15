466ad587cda9f0888bde98458212f1041d6e893e
Brock Fanning <brockfanning@gmail.com>
2019-03-14 23:59:28 -0400

    Fix for currentTime

--- _includes/assets/js/plugins/leaflet.yearSlider.js
+++ _includes/assets/js/plugins/leaflet.yearSlider.js
@@ -42,7 +42,7 @@
       // cause any problems. This converts the array of years into a comma-
       // delimited string of YYYY-MM-DD dates.
       times: options.years.join('-01-02,') + '-01-02',
-      currentTime: new Date(options.yearStart + '-01-02').getTime(),
+      currentTime: new Date(options.years[0] + '-01-02').getTime(),
     });
     // Create the player.
     options.player = new L.TimeDimension.Player(options.playerOptions, options.timeDimension);
