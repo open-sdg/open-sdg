574112e8956cbf339ab65878c9322ace097d949d
Brock Fanning <brockfanning@gmail.com>
2020-01-11 18:37:52 -0500

    Javascript cleanup

--- _includes/assets/js/search.js
+++ _includes/assets/js/search.js
@@ -1,45 +1,62 @@
-var urlParams = new URLSearchParams(window.location.search);
-var searchTerms = urlParams.get('q');
-document.getElementById('search-bar-on-page').value = searchTerms;
-document.getElementById('search-term').innerHTML = searchTerms;
-if (searchTerms) {
-  var searchIndex = lunr(function () {
-    this.ref('url');
-    this.field('title', { boost: 10 });
-    this.field('content');
-    this.field('hidden');
+var indicatorSearch = function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var searchTerms = urlParams.get('q');
+  if (searchTerms) {
+    document.getElementById('search-bar-on-page').value = searchTerms;
+    document.getElementById('search-term').innerHTML = searchTerms;
 
-    for (var ref in opensdg.searchItems) {
-      this.add(opensdg.searchItems[ref]);
-    };
-  });
+    var searchIndex = lunr(function () {
+      this.ref('url');
+      this.field('title', { boost: 10 });
+      this.field('content');
+      this.field('hidden');
 
-  var searchTermsToUse = searchTerms;
-  // This is to allow for searching by indicator with dashes.
-  if (searchTerms.split('-').length == 3 && searchTerms.length < 15) {
-    // Just a best-guess check to see if the user intended to search for an
-    // indicator ID.
-    searchTermsToUse = searchTerms.replace(/-/g, '.');
-  }
-  // Perform the search.
-  var results = searchIndex.search(searchTermsToUse);
-  var resultItems = [];
-  results.forEach(function(result) {
-    var doc = opensdg.searchItems[result.ref]
-    // Truncate the contents.
-    if (doc.content.length > 400) {
-      doc.content = doc.content.substring(0, 400) + '...';
+      for (var ref in opensdg.searchItems) {
+        this.add(opensdg.searchItems[ref]);
+      };
+    });
+
+    var searchTermsToUse = searchTerms;
+    // This is to allow for searching by indicator with dashes.
+    if (searchTerms.split('-').length == 3 && searchTerms.length < 15) {
+      // Just a best-guess check to see if the user intended to search for an
+      // indicator ID.
+      searchTermsToUse = searchTerms.replace(/-/g, '.');
     }
-    resultItems.push(doc);
+    // Perform the search.
+    var results = searchIndex.search(searchTermsToUse);
+    var resultItems = [];
+    results.forEach(function(result) {
+      var doc = opensdg.searchItems[result.ref]
+      // Truncate the contents.
+      if (doc.content.length > 400) {
+        doc.content = doc.content.substring(0, 400) + '...';
+      }
+      resultItems.push(doc);
+    });
+
+    // Print the results using a template.
+    var template = _.template(
+      $("script.results-template").html()
+    );
+
+    $('div.results').html(template({
+      searchResults: resultItems,
+      resultsCount: resultItems.length
+    }));
+  }
+};
+
+$(function() {
+
+  var $el = $('#indicator_search');
+  $('#jump-to-search').show();
+  $('#jump-to-search a').click(function() {
+    if($el.is(':hidden')) {
+      $('.navbar span[data-target="search"]').click();
+    }
+    $el.focus();
   });
 
-  // Print the results using a template.
-  var template = _.template(
-    $("script.results-template").html()
-  );
-
-  $('div.results').html(template({
-    searchResults: resultItems,
-    resultsCount: resultItems.length
-  }));
-}
+  indicatorSearch();
+});
