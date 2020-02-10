5ef74f4e1f19187adbcf9049223b6146e9247bb1
Brock Fanning <brockfanning@gmail.com>
2020-02-10 08:52:21 -0500

    Show a dropdown for choosing a disaggregation on the map.

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -16,4 +16,5 @@
 {%- include assets/js/lib/modernizr-custom.js -%}
 {%- include assets/js/plugins/leaflet.selectionLegend.js -%}
 {%- include assets/js/plugins/leaflet.yearSlider.js -%}
+{%- include assets/js/plugins/leaflet.disaggregationSelect.js -%}
 {%- include assets/js/googleAnalytics.js -%}
