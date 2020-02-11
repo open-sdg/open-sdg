490fb2da167e700914b67ccd106b024d0aac5f6a
Brock Fanning <brockfanning@gmail.com>
2020-02-11 09:45:25 -0500

    Tweaks

--- _includes/assets/js/plugins/leaflet.disaggregationSelect.js
+++ _includes/assets/js/plugins/leaflet.disaggregationSelect.js
@@ -26,16 +26,26 @@
     onAdd: function() {
       // Use the first feature - assumes they are all the same.
       var div = L.DomUtil.create('div', 'disaggregation-select-container'),
+          label = L.DomUtil.create('label', 'disaggregation-select-label', div),
           select = L.DomUtil.create('select', 'disaggregation-select', div),
           feature = this.plugin.getVisibleLayers().toGeoJSON().features[0],
           disaggregations = feature.properties.disaggregations,
           options = disaggregations.map(function(disaggregation) {
-            return Object.values(disaggregation).join(' - ');
+            return Object.values(disaggregation).filter(function(subcategory) {
+              return subcategory;
+            }).join(' - ');
           });
 
+      // If there are one or less options to select from, return empty elements.
+      if (options.length < 2) {
+        return div;
+      }
+
+      label.setAttribute('for', 'disaggregation-select-element');
+      label.innerHTML = translations.indicator.sub_categories;
+      select.setAttribute('id', 'disaggregation-select-element');
       select.innerHTML = options.map(function(option) {
-        var label = (option === '') ? translations.indicator.sub_categories : option;
-        return '<option>' + label  + '</option>';
+        return '<option>' + option  + '</option>';
       });
       var that = this;
       L.DomEvent.on(select, 'change', function(event) {
