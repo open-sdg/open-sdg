49b8b5159e8f3239abd8f258f9826ebcce3b5976
Brock Fanning <brockfanning@gmail.com>
2020-08-10 08:14:37 -0400

    Translate no-data message and wrap long lines

--- _includes/assets/js/chartjs/noDataMessage.js
+++ _includes/assets/js/chartjs/noDataMessage.js
@@ -1,3 +1,22 @@
+function getTextLinesOnCanvas(ctx, text, maxWidth) {
+  var words = text.split(" ");
+  var lines = [];
+  var currentLine = words[0];
+
+  for (var i = 1; i < words.length; i++) {
+      var word = words[i];
+      var width = ctx.measureText(currentLine + " " + word).width;
+      if (width < maxWidth) {
+          currentLine += " " + word;
+      } else {
+          lines.push(currentLine);
+          currentLine = word;
+      }
+  }
+  lines.push(currentLine);
+  return lines;
+}
+
 Chart.plugins.register({
   afterDraw: function(chart) {
     if (chart.data.datasets.length === 0) {
@@ -11,7 +30,15 @@ Chart.plugins.register({
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
       ctx.font = "normal 40px 'Open Sans', Helvetica, Arial, sans-serif";
-      ctx.fillText('No data to display', width / 2, height / 2);
+      var lines = getTextLinesOnCanvas(ctx, translations.indicator.data_not_available, chart.chart.width);
+      var numLines = lines.length;
+      var lineHeight = 50;
+      var xLine = width / 2;
+      var yLine = (height / 2) - ((lineHeight / 2) * numLines);
+      for (var i = 0; i < numLines; i++) {
+        ctx.fillText(lines[i], xLine, yLine);
+        yLine += lineHeight;
+      }
       ctx.restore();
     }
   }
