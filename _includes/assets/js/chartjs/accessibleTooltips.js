02e16ab73b54f216a632a43f29b3bdce00bb29b5
Brock Fanning <brockfanning@gmail.com>
2020-09-18 13:03:14 -0400

    Accessible chart tooltips

---
+++ _includes/assets/js/chartjs/accessibleTooltips.js
@@ -0,0 +1,117 @@
+// This plugin allows users to cycle through tooltips by keyboard.
+Chart.plugins.register({
+    afterInit: function(chart) {
+        var plugin = this;
+        plugin.chart = chart;
+        plugin.currentTooltip = null;
+        $(chart.canvas).keydown(function(e) {
+            switch (e.which) {
+                case 37:
+                    plugin.previousTooltip();
+                    e.preventDefault();
+                    break;
+                case 39:
+                    plugin.nextTooltip();
+                    e.preventDefault();
+                    break;
+            }
+        });
+    },
+    afterDatasetsDraw: function() {
+        var plugin = this;
+        if (plugin.allTooltips == null) {
+            plugin.allTooltips = plugin.getAllTooltips();
+        }
+    },
+    afterUpdate: function() {
+        var plugin = this;
+        plugin.allTooltips = null;
+        plugin.currentTooltip = null;
+    },
+    getAllTooltips: function() {
+        var datasets = this.chart.data.datasets;
+        var allTooltips = [];
+        // For line charts, we group points into vertical tooltips.
+        if (this.chart.config.type == 'line') {
+            for (var pointIndex = 0; pointIndex < datasets[0].data.length; pointIndex++) {
+                var verticalTooltips = [];
+                for (var datasetIndex = 0; datasetIndex < datasets.length; datasetIndex++) {
+                    if (datasets[datasetIndex].data[pointIndex] !== null) {
+                        verticalTooltips.push(this.chart.getDatasetMeta(datasetIndex).data[pointIndex]);
+                    }
+                }
+                if (verticalTooltips.length > 0) {
+                    allTooltips.push(verticalTooltips);
+                }
+            }
+        }
+        // For other charts, each point gets its own tooltip.
+        else {
+            for (var datasetIndex = 0; datasetIndex < datasets.length; datasetIndex++) {
+                for (var pointIndex = 0; pointIndex < datasets[datasetIndex].data.length; pointIndex++) {
+                    var singleTooltip = this.chart.getDatasetMeta(datasetIndex).data[pointIndex];
+                    allTooltips.push(singleTooltip);
+                }
+            } 
+        }
+        return allTooltips;
+    },
+    previousTooltip: function() {
+        var plugin = this,
+            newTooltip = 0;
+        if (plugin.currentTooltip !== null) {
+            newTooltip = plugin.currentTooltip - 1;
+        }
+        if (newTooltip < 0) {
+            newTooltip = plugin.allTooltips.length - 1;
+        }
+        plugin.activateTooltips(plugin.allTooltips[newTooltip]);
+        plugin.currentTooltip = newTooltip;
+    }, 
+    nextTooltip: function() {
+        var plugin = this,
+            newTooltip = 0;
+        if (plugin.currentTooltip !== null) {
+            newTooltip = plugin.currentTooltip + 1;
+        }
+        if (newTooltip >= plugin.allTooltips.length) {
+            newTooltip = 0;
+        }
+        plugin.activateTooltips(plugin.allTooltips[newTooltip]);
+        plugin.currentTooltip = newTooltip;
+    },
+    activateTooltips: function(tooltips) {
+        this.chart.tooltip._active = tooltips
+        this.chart.tooltip.update(true);
+        this.chart.draw();
+        this.announceTooltips(tooltips);
+    },
+    announceTooltips: function(tooltips) {
+        if (tooltips.length > 0) {
+            var labels = {};
+            for (var i = 0; i < tooltips.length; i++) { 
+                var datasetIndex = tooltips[i]._datasetIndex,
+                    pointIndex = tooltips[i]._index,
+                    year = this.chart.data.labels[pointIndex],
+                    dataset = this.chart.data.datasets[datasetIndex],
+                    label = dataset.label,
+                    value = dataset.data[pointIndex];
+                if (typeof labels[year] === 'undefined') {
+                    labels[year] = [];
+                }
+                labels[year].push(label + ' ' + value);
+            }
+            var announcement = '';
+            Object.keys(labels).forEach(function(year) {
+                announcement += year + ': ';
+                labels[year].forEach(function(label) {
+                    announcement += label + ', ';
+                });
+            });
+            var currentAnnouncement = $('#chart-tooltip-status').text();
+            if (currentAnnouncement != announcement) {
+                $('#chart-tooltip-status').text('Tooltip active: ' + announcement);
+            }
+        }
+    }
+});
