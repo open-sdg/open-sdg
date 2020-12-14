a390eb5cdcac9b4a8adf84e608ca7627755c72b8
Brock Fanning <brockfanning@gmail.com>
2020-12-14 14:58:56 -0500

    Only make unit and series configurable for now

--- _includes/assets/js/model/constants.js
+++ _includes/assets/js/model/constants.js
@@ -1,10 +1,10 @@
 /**
  * Constants to be used in indicatorModel.js and helper functions.
  */
-var UNIT_COLUMN = '{{ site.column_names.units | default: "Units" }}';
-var SERIES_COLUMN = '{{ site.column_names.series | default: "Series" }}';
+var UNIT_COLUMN = '{{ site.data_fields.units | default: "Units" }}';
+var SERIES_COLUMN = '{{ site.data_fields.series | default: "Series" }}';
 var GEOCODE_COLUMN = 'GeoCode';
-var YEAR_COLUMN = '{{ site.column_names.year | default: "Year" }}';
-var VALUE_COLUMN = '{{ site.column_names.value | default: "Value" }}';
+var YEAR_COLUMN = 'Year';
+var VALUE_COLUMN = 'Value';
 var HEADLINE_COLOR = '#777777';
 var SERIES_TOGGLE = {{ site.series_toggle | default: false }};
