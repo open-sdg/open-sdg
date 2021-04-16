6748d79b0ab55e22b7963a930ae47981a14d1d46
Moritz Bosse <50657629+MoBosse@users.noreply.github.com>
2021-04-16 07:57:09 +0200

    Update fieldHelpers.js

--- _includes/assets/js/model/fieldHelpers.js
+++ _includes/assets/js/model/fieldHelpers.js
@@ -269,8 +269,25 @@ function getCombinationData(fieldItems) {
   });
   fieldValuePairCombinations = Object.values(fieldValuePairCombinations);
 
-  // Return a combination of both.
-  return fieldValuePairs.concat(fieldValuePairCombinations);
+  var unsortedFieldValuePairCombinations = fieldValuePairs.concat(fieldValuePairCombinations);
+
+  // Due to the forEach loops above the Combinations are in a more or less random order right now.
+  // The following sorts the combinations depending on the order of the "fieldItems".
+  sortedFieldValuePairs = [];
+  console.log('unsortedComb:', unsortedFieldValuePairCombinations);
+  unsortedFieldValuePairCombinations.forEach(function(combination){
+    var combinations = {};
+    fieldItems.forEach(function(fieldItem) {
+      if (Object.keys(combination).indexOf(fieldItem.field) != -1){
+        var pair = {};
+        pair[fieldItem.field] = combination[fieldItem.field];
+        Object.assign(combinations, pair);
+      }
+    });
+    sortedFieldValuePairs.push(combinations);
+  });
+
+  return sortedFieldValuePairs;
 }
 
 /**
