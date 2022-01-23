f88f57722e964f53c0163067c901803d40ed81c7
Brock Fanning <brockfanning@gmail.com>
2022-01-23 18:55:50 -0500

    Refactor to allow version2 files

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1,6 +1,39 @@
 ---
 # Don't delete this line.
 ---
+{% if site.version_2_preview %}
+
+{%- include assets/js/autotrack-element.js -%}
+{%- include assets/js/plugins/jquery.sdgMap.js -%}
+{%- include assets/js/chartjs/rescaler.js -%}
+{%- include assets/js/chartjs/noDataMessage.js -%}
+{% if site.accessible_charts %}
+{%- include assets/js/chartjs/accessibleCharts.js -%}
+{% endif %}
+{%- include assets/js/event.js -%}
+{%- include assets/js/accessibility.js -%}
+{%- include assets/js/chartColors.js -%}
+{%- include assets/js/indicatorModel.js -%}
+{%- include assets/js/mapView.js -%}
+{%- include assets/js/indicatorView.js -%}
+{%- include assets/js/indicatorController.js -%}
+{%- include assets/js/indicatorInit.js -%}
+{%- include assets/js/tabs.js -%}
+{% if site.accessible_tabs %}
+{%- include assets/js/accessibleTabs.js -%}
+{% endif %}
+{%- include assets/js/search.js -%}
+{%- include assets/js/menu.js -%}
+{%- include assets/js/lib/classList.js -%}
+{%- include assets/js/lib/modernizr-custom.js -%}
+{%- include assets/js/plugins/leaflet.selectionLegend.js -%}
+{%- include assets/js/plugins/leaflet.yearSlider.js -%}
+{%- include assets/js/plugins/leaflet.fullscreenAccessible.js -%}
+{%- include assets/js/plugins/leaflet.searchAccessible.js -%}
+{%- include assets/js/backToTop.js -%}
+
+{% else %}
+
 {%- include assets/js/autotrack-element.js -%}
 {%- include assets/js/plugins/jquery.sdgMap.js -%}
 {%- include assets/js/chartjs/rescaler.js -%}
@@ -28,3 +61,5 @@
 {%- include assets/js/plugins/leaflet.fullscreenAccessible.js -%}
 {%- include assets/js/plugins/leaflet.searchAccessible.js -%}
 {%- include assets/js/backToTop.js -%}
+
+{% endif %}
