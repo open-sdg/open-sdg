81a463c4b5b051a4f813e14e7506fedcce6615eb
Brock Fanning <brockfanning@gmail.com>
2022-02-21 07:03:01 -0500

    Avoid border issue on binary bars

--- _includes/assets/js/view/chartTypeBinary.js
+++ _includes/assets/js/view/chartTypeBinary.js
@@ -56,6 +56,11 @@ opensdg.chartTypes.binary = function (info) {
         return dataset;
     });
 
+    // Manually set the borderWidths to 0 to avoid a weird border effect on the bars.
+    config.data.datasets.forEach(function(dataset) {
+        dataset.borderWidth = 0;
+    });
+
     // Add these overrides onto the normal config.
     _.merge(config, overrides);
     return config;
