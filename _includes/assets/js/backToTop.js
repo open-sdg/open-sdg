53bdd83f7f55f0442d77a7d5cea9297cb260f973
Brock Fanning <brockfanning@gmail.com>
2022-03-11 08:24:21 -0500

    Before reverting bootstrap icons

--- _includes/assets/js/backToTop.js
+++ _includes/assets/js/backToTop.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('a[href="#top"]').prepend('<i class="fa fa-arrow-up"></i>');
+    $('a[href="#top"]').prepend('<i class="{% if site.bootstrap_5 %}bi bi-arrow-up{% else %}fa fa-arrow-up{% endif %}"></i>');
 });
--- _includes/assets/js/plugins/leaflet.fullscreenAccessible.js
+++ _includes/assets/js/plugins/leaflet.fullscreenAccessible.js
@@ -16,14 +16,19 @@
             var container = L.Control.Fullscreen.prototype.onAdd.call(this, map);
             this.link.setAttribute('role', 'button');
             this.link.setAttribute('aria-label', this.link.title);
-            this.link.innerHTML = '<i class="fa fa-expand" aria-hidden="true"></i>';
+            this.link.innerHTML = '<i class="{% if site.bootstrap_5 %}bi bi-fullscreen{% else %}fa fa-expand{% endif %}" aria-hidden="true"></i>';
             return container;
         },
         _toggleTitle: function() {
             L.Control.Fullscreen.prototype._toggleTitle.call(this);
             this.link.setAttribute('aria-label', this.link.title);
+            {% if site.bootstrap_5 %}
+            var biClass = this._map.isFullscreen() ? 'bi-fullscreen-exit' : 'bi-fullscreen';
+            this.link.innerHTML = '<i class="bi ' + biClass + '" aria-hidden="true"></i>';
+            {% else %}
             var faClass = this._map.isFullscreen() ? 'fa-compress' : 'fa-expand'
             this.link.innerHTML = '<i class="fa ' + faClass + '" aria-hidden="true"></i>';
+            {% endif %}
         }
     });
 
