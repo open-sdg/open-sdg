f1554255797290178229cef965ac0a775c200f08
Brock Fanning <brockfanning@gmail.com>
2022-06-07 08:07:54 -0400

    SVG approach from GDS

--- _includes/assets/js/backToTop.js
+++ _includes/assets/js/backToTop.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('a[href="#top"]').prepend('<i class="fa fa-long-arrow-up"></i>');
+    $('a[href="#top"]').prepend('<svg class="app-c-back-to-top__icon" xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" aria-hidden="true" focusable="false"><path fill="currentColor" d="M6.5 0L0 6.5 1.4 8l4-4v12.7h2V4l4.3 4L13 6.4z"></path></svg>');
 });
