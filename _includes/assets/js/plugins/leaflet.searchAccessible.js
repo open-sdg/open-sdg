9e71266069c1bd6d3590fc06c570935528db5359
Brock Fanning <brockfanning@gmail.com>
2022-10-20 11:52:38 -0400

    Use more fuzzy searching for maps

--- _includes/assets/js/plugins/leaflet.searchAccessible.js
+++ _includes/assets/js/plugins/leaflet.searchAccessible.js
@@ -103,6 +103,17 @@
       if ((typeof e === 'undefined' || e.type === 'keyup') && this._input.value === '') {
         return;
       }
+      if (this._tooltip.childNodes.length > 0 && this._input.value !== '') {
+        // This is a workaround for the bug where non-exact matches
+        // do not successfully search. See this Github issue:
+        // https://github.com/stefanocudini/leaflet-search/issues/264
+        var firstSuggestion = this._tooltip.childNodes[0].innerText;
+        var firstSuggestionLower = firstSuggestion.toLowerCase();
+        var userInputLower = this._input.value.toLowerCase();
+        if (firstSuggestionLower !== userInputLower && firstSuggestionLower.includes(userInputLower)) {
+          this._input.value = firstSuggestion;
+        }
+      }
       L.Control.Search.prototype._handleSubmit.call(this, e);
     },
     _handleArrowSelect: function(velocity) {
