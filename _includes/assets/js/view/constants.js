ade2cbff083c53a03b9c64e5167b3b6aee565703
Brock Fanning <brockfanning@gmail.com>
2023-02-20 23:45:39 -0500

    Allow for site config to control proxy pill label and description

--- _includes/assets/js/view/constants.js
+++ _includes/assets/js/view/constants.js
@@ -1,3 +1,3 @@
 var HIDE_SINGLE_SERIES = {{ site.hide_single_series | default: false }};
 var HIDE_SINGLE_UNIT = {{ site.hide_single_unit | default: false }};
-var PROXY_PILL = '<span aria-describedby="proxy-description" class="proxy-pill">' + translations.indicator.proxy + '</span>';
+var PROXY_PILL = '<span aria-describedby="proxy-description" class="proxy-pill">' + translations.t({{ site.proxy_indicators.label | default: "indicator.proxy" | jsonify }}) + '</span>';
